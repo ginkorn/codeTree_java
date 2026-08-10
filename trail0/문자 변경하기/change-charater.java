c9630129ff89e0e8713d65679cc8d84ec7f46bc0
전현진 <jhs000207@naver.com>
2026-08-10 11:43:07 +0900

    [Passed] 문자 변경하기 (92ms, 9MB)

---
+++ trail0/문자 변경하기/change-charater.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        char c = 'A';
+
+        c = 'T';
+
+        System.out.println(c);
+    }
+}
