3a2b8673e7099c0e6d4d0086923dea8ff07cee79
전현진 <jhs000207@naver.com>
2026-08-10 14:09:15 +0900

    [Compile Error] 입력받은 값과 합 출력

---
+++ trail0/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+
+       int sum = 0;
+       sum = a+b;
+
+        System.out.println(a+ " " +b+ " "+ sum);
+    }
+    }
+}
