2901b10965ed4bbb8142eeab4ca594d4a6cbebf4
전현진 <jhs000207@naver.com>
2026-08-10 15:33:25 +0900

    [Passed] 비교 연산 (149ms, 10MB)

--- trail0/비교 연산/comparison-operator.java
+++ trail0/비교 연산/comparison-operator.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
-    
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-         System.out.println(a >= b);
-         System.out.println(a > b);
-         System.out.println(a <= b);
-         System.out.println(a < b);
-         System.out.println(a == b);
-         System.out.println(a != b);
-
-    
+        System.out.println((a >= b) ? 1 : 0);   // A가 B보다 같거나 큰가?
+        System.out.println((a > b) ? 1 : 0);    // A가 B보다 큰가?
+        System.out.println((b >= a) ? 1 : 0);   // B가 A보다 같거나 큰가?
+        System.out.println((b > a) ? 1 : 0);    // B가 A보다 큰가?
+        System.out.println((a == b) ? 1 : 0);   // A와 B가 같은가?
+        System.out.println((a != b) ? 1 : 0);   // A와 B가 다른가?
     }
 }
